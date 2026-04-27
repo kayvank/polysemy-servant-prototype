209f3baed0151ba00f5a886fb7b73dea99261532
Kayvan  ≅ کیوان <kayvan@q2io.com>
2026-04-27 09:27:36 -0700

    Add User routes (#4)
    * Add User routes
    
    * Update src/www/src/lib/API/Routes.hs
    
    Co-authored-by: William Yao <williamyaoh@gmail.com>
    
    * Group servant routes
    
    Co-authored-by: williamyaoh
    
    ---------
    
    Co-authored-by: William Yao <williamyaoh@gmail.com>

--- src/www/src/lib/API/Routes.hs
+++ src/www/src/lib/API/Routes.hs
@@ -24,16 +24,13 @@ import Servant (
 
 -- | Top-level API type
 type API =
-  -- "users" :> UserAPI :<|>
-  "items" :> LineItemAPI
+  "users" :> UserAPI :<|> "items" :> LineItemAPI
 
 type UserAPI =
-  "users"
-    :> ( Get '[JSON] (ApiResponse [User])
-           :<|> Capture "id" Int :> Get '[JSON] (ApiResponse User)
-           :<|> ReqBody '[JSON] NewUser :> Post '[JSON] ()
-           :<|> Capture "id" Int :> ReqBody '[JSON] NewUser :> Put '[JSON] (ApiResponse User) -- PUT  /users/:id
-       )
+  Get '[JSON] (ApiResponse [User])
+    :<|> Capture "id" Int :> Get '[JSON] (ApiResponse User)
+    :<|> ReqBody '[JSON] NewUser :> Post '[JSON] (ApiResponse User)
+    :<|> Capture "id" Int :> ReqBody '[JSON] NewUser :> Put '[JSON] (ApiResponse User) -- PUT  /users/:id
 
 -- | Route definitions
 type LineItemAPI =
@@ -42,19 +39,3 @@ type LineItemAPI =
     :<|> ReqBody '[JSON] NewLineItem :> Post '[JSON] (ApiResponse LineItem)
     :<|> Capture "id" Int :> ReqBody '[JSON] NewLineItem :> Put '[JSON] (ApiResponse LineItem) -- PUT  /lineitems/:id
     :<|> Capture "id" Int :> Delete '[JSON] (ApiResponse Bool) -- DEL  /items/:id
-
-{-
-type LineItemAPI =
-  "items"
-    :> Get '[JSON] (ApiResponse [LineItem])
-    :<|> ReqBody '[JSON] NewLineItem :> Post '[JSON] (ApiResponse LineItem)
-    :<|> Capture "id" Int :> Get '[JSON] (ApiResponse LineItem)
-    :<|> Capture "id" Int :> Delete '[JSON] (ApiResponse Bool) -- DEL  /items/:id
-    :<|> Capture "id" Int :> ReqBody '[JSON] NewLineItem :> Put '[JSON] (ApiResponse LineItem) -- PUT  /lineitems/:id
-
-    :<|> "users" :> Get '[JSON] (ApiResponse [User])
-    :<|> Capture "id" Int :> Get '[JSON] (ApiResponse User)
-    :<|> ReqBody '[JSON] NewUser :> Post '[JSON] ()
-    :<|> Capture "id" Int :> ReqBody '[JSON] NewUser :> Put '[JSON] (ApiResponse User) -- PUT  /users/:id
--}
--- type UserAPI = "users" :> Get '[JSON] (ApiResponse [User])
